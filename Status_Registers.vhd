e87b1e4aeefd16c57116d68b4a9d2e126e6032c4
Dhruva Dhingra <dhruvadhingra@cse.iitb.ac.in>
2021-05-02 21:04:21 +0530

    Removed alu_to_status_reg and made Status_Registers.vhd

--- Status_Registers.vhd
+++ Status_Registers.vhd
@@ -9,11 +9,14 @@ use ieee.numeric_std.all;
 
 entity Status_Register is 
 port (
-D_in : in std_logic;
-R_in : in std_logic;
+C_in : in std_logic;
+Z_in : in std_logic;
+C_alu : in std_logic;
+Z_alu : in std_logic;
 reg_read : in std_logic;
 reg_write : in std_logic;
-D_out : out std_logic;
+C_out : in std_logic;
+Z_out : in std_logic;
 clk : in std_logic;
 )
 
@@ -27,9 +30,12 @@ begin
 	
 	if(rising_edge(clk)) then
 		if (reg_read = '1') then
-			D_out <= registers(to_integer(unsigned(R_in)));
+			C_out <= registers(0);
+			Z_out <= registers(1);
 		elsif (reg_write = '1') then
-			registers(to_integer(unsigned(R_in))) <= D_in;
+			to_update <= ((C_in = '0' and Z_in = '0') or (C_in = '1' and registers(0) = '1' and Z_in = '0') or (Z_in = '1' and registers(1) = '1' and C_in = '0'));
+			registers(0) <= ( (to_update and C_alu) or ( not(to_update) and registers(0) ) );
+			registers(1) <= ( (to_update and Z_alu) or ( not(to_update) and registers(1) ) );
 		end if;
 	end if;
 	end process;
